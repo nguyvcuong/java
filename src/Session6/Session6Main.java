a1367ec6eccb1f26a77c9881d2dc32e81b39ee09
nguyvcuong <110459436+nguyvcuong@users.noreply.github.com>
2023-02-01 13:45:52 +0700

    bt6

--- src/Session6/Session6Main.java
+++ src/Session6/Session6Main.java
@@ -1,2 +1,25 @@
-package Session6;public class Session6Main {
+package Session6;
+
+import Session6.American;
+import Session6.Asian;
+import Session6.Human;
+
+public class Session6Main {
+    public static void main(String[]args){
+        // Human = new Human ();ko the tao dc doi tuong tu lop truu tuong
+        Asian a = new Asian();
+        American an = new American();
+        Human h = new Human() { //anonynous class ko ten
+            @Override
+            public void eat() {
+
+            }
+
+            @Override
+            public void sleep() {
+
+            }
+        };
+
+    }
 }
