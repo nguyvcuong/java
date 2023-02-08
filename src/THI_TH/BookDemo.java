41a884b2b381a8b804d0d6724255eaae37e5bee8
nguyvcuong <110459436+nguyvcuong@users.noreply.github.com>
2023-02-08 15:33:06 +0700

    thi thuc hanh

---
+++ src/THI_TH/BookDemo.java
@@ -0,0 +1,9 @@
+package THI_TH;
+
+public class BookDemo {
+    public static void main(String []args){
+        Author author = new Author("Russel", "Winderand");
+        Book book = new Book("Developing Java Software","Russel Winderand", 79.75);
+        System.out.println(book);
+    }
+}
